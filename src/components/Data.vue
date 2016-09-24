14866125344016a52dba6a598bb76903a5681cfb
immunochomik <tomaszswider4@gmail.com>
2016-09-24 22:12:23 +0100

    import and export of data to file implemented

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -16,7 +16,7 @@
           <button class="btn btn-default quoter" @click="find">Find</button>
           <button class="btn btn-default quoter">Insert</button>
           <button class="btn btn-default quoter">Update</button>
-          <button class="btn btn-danger quoter">Delete</button>
+          <button class="btn btn-danger quoter" @click="deleteItems">Delete</button>
         </div>
       </div>
       <div class="panel-body col-sm-6">
@@ -25,19 +25,30 @@
       </div>
     </div>
     <div class="panel-body">
-      <pre class="pre-scrollable" style="min-height: 30em;">{{ fullData }}</pre>
+      <pre class="pre-scrollable" style="min-height: 30em;">{{ fullDataString }}</pre>
     </div>
-    <div class="panel-body">
-      <button @click="import" class="btn btn-default">Import</button>
-      <button @click="import" class="btn btn-default">Export</button>
+    <div class="panel-body row">
+      <div class="col-sm-6 col-md-6">
+        <input class="form-control" type="file" id="files" name="files[]" multiple />
+      </div>
+
+      <div class="col-sm-6 col-md-6">
+        <button @click="importItems" class="btn btn-default">Import</button>
+        <button @click="export" class="btn btn-default">Export</button>
+        <button @click="deleteAll" class="btn btn-danger">Delete All</button>
+        <button @click="compact" class="btn btn-danger">Compact</button>
+      </div>
+
     </div>
   </div>
 </template>
 <script>
+  const dbName = 'tasks2';
   import StoreCollection from '../storeCollection';
   import Vue from 'vue';
   var _ = require('lodash');
-  var store = new StoreCollection.Collection('tasks2');
+  var store = new StoreCollection.Collection(dbName);
+
 
   function findInDoc(doc, key, expected, compare) {
     if(key.indexOf('.') == -1) {
@@ -54,11 +65,21 @@
     return findInDoc(inDock, keyParts.join('.'), expected)
   }
 
+  function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  }
+
   export default {
     name: 'Data',
     data: function() {
       return {
-        fullData : '',
+        fullData : {},
         update: '{}',
         query: '{}',
       }
@@ -70,6 +91,45 @@
       }
     },
     methods : {
+      compact : function() {
+        store.compact();
+      },
+      deleteItems : function() {
+        store.all().then(res => {
+          var data = this.filterFind(res.rows);
+          if(data) {
+            console.log(data);
+            data.forEach(function(item) {
+              store.removeById(item.id);
+            })
+          }
+        }).catch(err => {
+          console.log(err);
+        });
+      },
+      importItems: function() {
+        var input = el('files');
+        var reader = new FileReader();
+        reader.onload =(function(theFile) {
+          return function(e) {
+            var data = JSON.parse(e.target.result);
+            if(data) {
+              data.forEach(function(item) {
+                delete item.doc._rev;
+                store.put(item.doc).then(res => {
+                    console.log(res);
+                }).catch(err => {
+                    console.log('Error', err);
+                });
+              });
+            }
+          }
+        })(input.files[0]);
+        reader.readAsText(input.files[0]);
+      },
+      export : function() {
+        download('export_{0}.json'.f([today()]), JSON.stringify(this.fullData));
+      },
       find: function() {
         this.refresh();
       },
@@ -89,16 +149,24 @@
         return docs;
       },
       refresh : function() {
-
+        console.log(store);
         store.all().then(res => {
-          this.fullData = JSON.stringify(this.filterFind(res.rows), null, 2);
+          this.fullData = this.filterFind(res.rows);
         }).catch(err => {
-          console.log('Error', err);
+          if(err.message === 'database is destroyed') {
+            store = new StoreCollection.Collection(dbName);
+            store.all().then(res => {
+              this.fullData = this.filterFind(res.rows);
+            })
+          } else {
+            console.log(err);
+          }
         });
         console.log('Refreshed');
       },
-      destroy : function() {
+      deleteAll : function() {
         if(confirm('Destroy ?')) {
+          download('snapshot_{0}.json'.f([today()]), JSON.stringify(this.fullData));
           console.log('Destroy');
           store.destroy();
           this.refresh();
@@ -111,6 +179,11 @@
       doStuff: function() {
         //return;
         Vue.util.warn('Hello');
+      }
+    },
+    computed : {
+      fullDataString: function() {
+        return JSON.stringify(this.fullData, null, 2);
       }
     }
 
